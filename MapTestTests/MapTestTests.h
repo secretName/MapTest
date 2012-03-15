4e3bee233460459dbd1f4b96d9ebc6ec0a291b6d
ryan <ryan@dyn-72-33-193-79.uwnet.wisc.edu>
2012-03-15 12:19:10 -0500

    Initial Commit

---
+++ MapTestTests/MapTestTests.h
@@ -0,0 +1,13 @@
+//
+//  MapTestTests.h
+//  MapTestTests
+//
+//  Created by Ryan Shenk on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MapTestTests : SenTestCase
+
+@end
---
+++ MapTestTests/MapTestTests.m
@@ -0,0 +1,32 @@
+//
+//  MapTestTests.m
+//  MapTestTests
+//
+//  Created by Ryan Shenk on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapTestTests.h"
+
+@implementation MapTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapTestTests");
+}
+
+@end
